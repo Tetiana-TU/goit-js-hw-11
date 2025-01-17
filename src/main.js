4e8d9397c357ccbe81620c7a9f3c9077285c26b2
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-01-17 14:07:35 +0200

    homework11 import export

--- src/main.js
+++ src/main.js
@@ -2,29 +2,22 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import {createGalleryCardTemplate} from "./js/render-functions";
+import {fetchPhotosByQuery} from "./js/pixabay-api";
+
 
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
 
-const createGalleryCardTemplate = imgInfo => {
-  return `<li class="gallery-card">
-   <img class="gallery-img"src="${imgInfo.webformatURL}" alt="${imgInfo.tags}"/>
-    </li>`;
-};
-
 const onSearchFormSubmit = event => {
   event.preventDefault();
-  const searchedQuery = event.currentTarget.elements.user_query.value;
 
-  fetch(
-    `https://pixabay.com/api/?key=48265594-3edacf02e8cadda91195713cc&q=${searchedQuery}&image_type=photo&orientation=horizontal&safesearch=true`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
+  const searchedQuery = event.currentTarget.elements.user_query.value.trim();
+  if (searchedQuery === '') {
+    alert ('Поле має бути заповнено!');
+    return;
+  }
+  fetchPhotosByQuery(searchedQuery)
     .then(data => {
       if (data.total === 0) {
         iziToast.error({
